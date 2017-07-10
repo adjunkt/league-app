bf1bf2ca81caa8212e4873523afd7885e7aa2658
ksure <kyle.scherling@gmail.com>
2017-07-09 19:06:47 -0700

    Install mocha/chai/eslint

---
+++ app/routes/organization.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+const {
+  Route,
+  get
+} = Ember
+
+export default Route.extend({
+  model(params) {
+    return this.get('store')
+      .query('organization', { friendlyUrl: get(params, 'organization_path')})
+      .then(result => get(result, 'firstObject'))
+  }
+});
