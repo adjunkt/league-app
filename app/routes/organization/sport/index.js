416c2710fd2e809e258f8713d80a02d93a9b5727
ksure <kyle.scherling@gmail.com>
2017-07-16 23:45:31 -0700

    Adds season route/templates and adds to router

--- app/routes/organization/sport/index.js
+++ app/routes/organization/sport/index.js
@@ -7,9 +7,9 @@ const {
 
 export default Route.extend({
   model() {
-    // const sportId = get(this.modelFor('sport'), 'id')
-    // const sport = this.get('store').peekRecord('sport', sportId)
-    //
-    // return sport.get('leagues')
+    const sportId = get(this.modelFor('organization.sport'), 'id')
+    const sport = this.get('store').peekRecord('sport', sportId)
+
+    return sport.get('leagues')
   }
 })
