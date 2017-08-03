b6f7e96ab343bec9ec65cf9263336ab6942cf208
ksure <kyle.scherling@gmail.com>
2017-08-03 08:10:08 -0700

    Remove make request custom override cause defaults and standards :)

--- app/authenticators/oauth2-password-grant.js
+++ app/authenticators/oauth2-password-grant.js
@@ -11,33 +11,4 @@ const {
 export default OAuth2PasswordGrant.extend({
   serverTokenEndpoint: `${ENV.apiUrl}/oauth/token`,
   serverTokenRevocationEndpoint: `${ENV.apiUrl}/oauth/revoke`,
-
-  makeRequest(url, data, headers = {}) {
-    const body = Object.keys(data).map(key =>
-      `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
-    ).join('&')
-
-    const options = {
-      body,
-      headers,
-      method: 'POST',
-      credentials: 'include'
-    }
-
-    options.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-
-    return new RSVP.Promise((resolve, reject) => {
-      fetch(url, options).then(response => {
-        response.text().then(text => {
-          const json = text ? JSON.parse(text) : {}
-          if (response.ok) {
-            resolve(json)
-          } else {
-            response.responseJSON = json
-            reject(response)
-          }
-        })
-      }).catch(reject)
-    })
-  }
 })
